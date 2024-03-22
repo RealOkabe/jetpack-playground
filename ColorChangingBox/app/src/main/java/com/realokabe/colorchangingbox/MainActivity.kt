692e09c61737a1b1e30a45728ad686125ae351d9
RealOkabe <anmolzagrawal@gmail.com>
2024-03-22 13:41:35 -0400

    Just made the box cover the screen and change color on click
    Right now the box is clickable and will change color when clicked. It will always change to a random color.

--- ColorChangingBox/app/src/main/java/com/realokabe/colorchangingbox/MainActivity.kt
+++ ColorChangingBox/app/src/main/java/com/realokabe/colorchangingbox/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
@@ -11,17 +12,19 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.realokabe.colorchangingbox.ui.theme.ColorChangingBoxTheme
+import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ColorChangingBoxTheme {
-
+                AddColorChangingBox(Modifier.fillMaxSize())
             }
         }
     }
@@ -30,9 +33,19 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun AddColorChangingBox(modifier: Modifier = Modifier) {
 
-    val color = mutableStateOf(Color.Red)
+    val color = remember {
+        mutableStateOf(Color.Red)
+    }
     
-    Box(modifier = Modifier
-        .background(color = color)
+    Box(modifier = modifier
+        .background(color = color.value)
+        .clickable {
+            color.value = Color(
+                Random.nextFloat(),
+                Random.nextFloat(),
+                Random.nextFloat(),
+                1f
+            )
+        }
     )
 }
